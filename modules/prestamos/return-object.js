fb0b9a398096e2f0c825a2745bb6b0044322245e
martha.marquez <martha.marquez@alumnos.uneatlantico.es>
2021-07-08 14:45:29 +0200

    Defectuosos

--- modules/prestamos/return-object.js
+++ modules/prestamos/return-object.js
@@ -1,17 +1,33 @@
 module.exports = function({connection, req}){
     return new Promise((resolve, reject)=>{
         const { id } = req.params;
-        const {usuario_cerro} = req.body;
-        const query = `UPDATE tb_prestamos SET devuelto = ?, hora_devolucion = NOW(), usuario_cerro = ? WHERE id = ?`;
-        // console.log(nombres, apellidos, email, dni, id_objeto, motivo, devuelto, firma, tiempo_prestamo, hora_prestamo, hora_devolucion, usuario_creo);
-        connection.query(query, [1, usuario_cerro, id], (ex,{rows}) => {
-            if (ex) {
-              console.log(ex);
-              reject({msg: "Error en consulta en DB"})
-            } else {
-              console.log("Rows: ",rows);
-              resolve({status: `El objeto del préstamo con id: ${id} ha sido devuelto exitosamente!`})
-            }
-        })
+        const {entrega_defectuosa, usuario_cerro} = req.body;
+
+        if(entrega_defectuosa === 0){
+          const query = `UPDATE tb_prestamos SET devuelto = ?, entrega_defectuosa = ?, hora_devolucion = NOW(), usuario_cerro = ? WHERE id = ?`;
+          // console.log(nombres, apellidos, email, dni, id_objeto, motivo, devuelto, firma, tiempo_prestamo, hora_prestamo, hora_devolucion, usuario_creo);
+          connection.query(query, [1, entrega_defectuosa, usuario_cerro, id], (ex,{rows}) => {
+              if (ex) {
+                console.log(ex);
+                reject({msg: "Error en consulta en DB"})
+              } else {
+                console.log("Rows: ",rows);
+                resolve({status: `El objeto del préstamo con id: ${id} ha sido devuelto exitosamente!`})
+              }
+          })
+        }else{
+          const query = `UPDATE tb_prestamos SET entrega_defectuosa = ? WHERE id = ?`;
+          // console.log(nombres, apellidos, email, dni, id_objeto, motivo, devuelto, firma, tiempo_prestamo, hora_prestamo, hora_devolucion, usuario_creo);
+          connection.query(query, [entrega_defectuosa, id], (ex,{rows}) => {
+              if (ex) {
+                console.log(ex);
+                reject({msg: "Error en consulta en DB"})
+              } else {
+                console.log("Rows: ",rows);
+                resolve({status: `El objeto del préstamo con id: ${id} ha sido devuelto defectuoso!`})
+              }
+          })
+        }
+
     });
 } 
