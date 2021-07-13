09af3ce691644935a498e0147c9ec35911aca109
martha.marquez <martha.marquez@alumnos.uneatlantico.es>
2021-07-13 14:06:56 +0200

    repaired objects

--- modules/defectiveObjects/add-defective-object.js
+++ modules/defectiveObjects/add-defective-object.js
@@ -2,9 +2,9 @@
 module.exports = function({connection, req}){
     return new Promise((resolve, reject)=>{
         const {id_objeto, motivo, cantidad, usuario_creo} = req.body;
-        const query = `INSERT INTO tb_defectuosos(id_objeto, motivo, cantidad, usuario_creo, fecha_creo) VALUES(?, ?, ?, ?, NOW())`;
+        const query = `INSERT INTO tb_defectuosos(id_objeto, motivo, cantidad, reparado, usuario_creo, fecha_creo) VALUES(?, ?, ?, ?, ?, NOW())`;
         // console.log(objeto, descripcion, cantidad, id_importancia, id_categoria, activo, usuario_creo, usuario_modifico, usuario_elimino, fecha_creo, fecha_modifico, fecha_elimino);
-        connection.query(query, [id_objeto, motivo, cantidad, usuario_creo], (ex,{rows}) => {
+        connection.query(query, [id_objeto, motivo, cantidad, 0, usuario_creo], (ex,{rows}) => {
             if (ex) {
               console.log(ex);
               reject({msg: "Error en consulta en DB"})
--- modules/defectiveObjects/update-defective-object.js
+++ modules/defectiveObjects/update-defective-object.js
@@ -1,17 +1,34 @@
 module.exports = function({connection, req}){
     return new Promise((resolve, reject)=>{
       const { id } = req.params;
-      const {id_objeto, motivo, cantidad, usuario_creo} = req.body;
-      const query = `UPDATE tb_defectuosos SET id_objeto = ?, motivo = ?, cantidad = ?, usuario_creo = ? WHERE id = ?`;
-      // console.log(categoria, usuario_creo);
-      connection.query(query, [id_objeto, motivo, cantidad, usuario_creo, id], (ex,{rows}) => {
-          if (ex) {
-            console.log(ex);
-            reject({msg: "Error en consulta en DB"})
-          } else {
-            console.log("Rows: ",rows);
-            resolve({status: `Objeto defectuoso con id: ${id} ha sido modificado exitosamente!`})
-          }
-      })
+      const {id_objeto, motivo, cantidad, reparado, usuario_creo} = req.body;
+      
+      if(reparado === 0){
+
+        const query = `UPDATE tb_defectuosos SET id_objeto = ?, motivo = ?, cantidad = ?, reparado = ?, usuario_creo = ? WHERE id = ?`;
+        // console.log(categoria, usuario_creo);
+        connection.query(query, [id_objeto, motivo, cantidad, reparado, usuario_creo, id], (ex,{rows}) => {
+            if (ex) {
+              console.log(ex);
+              reject({msg: "Error en consulta en DB"})
+            } else {
+              console.log("Rows: ",rows);
+              resolve({status: `Objeto defectuoso con id: ${id} ha sido modificado exitosamente!`})
+            }
+        })
+      }else{
+
+        const query = `UPDATE tb_defectuosos SET reparado = ? WHERE id = ?`;
+        // console.log(categoria, usuario_creo);
+        connection.query(query, [reparado, id], (ex,{rows}) => {
+            if (ex) {
+              console.log(ex);
+              reject({msg: "Error en consulta en DB"})
+            } else {
+              console.log("Rows: ",rows);
+              resolve({status: `Objeto defectuoso con id: ${id} ha sido reparado exitosamente!`})
+            }
+        })
+      }
     })
 }
