c2ad17c0f9754206721a40c8969efbbb6ad3a53b
martha.marquez <martha.marquez@alumnos.uneatlantico.es>
2021-07-19 14:27:22 +0200

    Masks

--- modules/defectiveObjects/update-defective-object.js
+++ modules/defectiveObjects/update-defective-object.js
@@ -1,13 +1,13 @@
 module.exports = function({connection, req}){
     return new Promise((resolve, reject)=>{
       const { id } = req.params;
-      const {id_objeto, motivo, cantidad, reparado, usuario_creo} = req.body;
+      const {id_objeto, motivo, cantidad, reparado} = req.body;
       
       if(reparado === 0){
 
-        const query = `UPDATE tb_defectuosos SET id_objeto = ?, motivo = ?, cantidad = ?, reparado = ?, usuario_creo = ? WHERE id = ?`;
+        const query = `UPDATE tb_defectuosos SET id_objeto = ?, motivo = ?, cantidad = ?, reparado = ? WHERE id = ?`;
         // console.log(categoria, usuario_creo);
-        connection.query(query, [id_objeto, motivo, cantidad, reparado, usuario_creo, id], (ex,{rows}) => {
+        connection.query(query, [id_objeto, motivo, cantidad, reparado, id], (ex,{rows}) => {
             if (ex) {
               console.log(ex);
               reject({msg: "Error en consulta en DB"})
