e2b1dafdf64df962edbdfb8c3557a20f8579ca90
martha.marquez <martha.marquez@alumnos.uneatlantico.es>
2021-07-27 14:08:17 +0200

    Prestamos Charts

--- modules/prestamos/list-prestamos.js
+++ modules/prestamos/list-prestamos.js
@@ -22,21 +22,19 @@ module.exports = function({connection, req}){
             }
         })
     }else{
-        const query = `SELECT * FROM tb_prestamos WHERE hora_prestamo BETWEEN ? AND ?`;
+        // const query = `SELECT * FROM tb_prestamos WHERE hora_prestamo BETWEEN ? AND ?`;
+        // const query2 = `SELECT COUNT(id_objeto) FROM tb_prestamos WHERE id_objeto = ?`;
+        // const query = `SELECT COUNT(id_objeto) as prestamos FROM tb_prestamos WHERE hora_prestamo BETWEEN ? AND ?`
+        const query = `SELECT obj.objeto as objeto, COUNT( id_objeto ) as prestamos FROM tb_prestamos prest, tb_objetos obj WHERE prest.id_objeto = obj.id AND hora_prestamo BETWEEN ? AND ? GROUP BY id_objeto`
         console.log(start_date, end_date);
         connection.query(query, [start_date, end_date], (ex, {rows})=>{
             if(ex){
                 console.log(ex);
                 reject({msg: 'Error en la consulta a la DB'});
             }else{
-                let arrObj = [];
-                rows.forEach(row => {
-                    arrObj.push(row.id_objeto)
-                });
-                console.log("Rows: ", arrObj);
                 resolve({
                     data: rows,
-                    msg: `Listado de prestamos en las fechas ${start_date} AND ${end_date}!`
+                    msg: `Listado de prestamos entre las fechas ${start_date} y ${end_date}!`
                 });
             }
         })
