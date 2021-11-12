20e9822e12926f96c01e653b422a733e36e5fa77
martha.marquez <martha.marquez@alumnos.uneatlantico.es>
2021-11-12 10:18:57 +0100

    sideBar Container

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -19,26 +19,38 @@ class sideBar extends Component {
     
         this.state = {
             menu: true,
-            responsive: false,
-            isActive: true
+            responsive: false
         }
         this.toggleMenu = this.toggleMenu.bind(this);
+        // console.log(window.innerWidth);
+        if(window.innerWidth >= 770){
+
+        }
+            // const show = (this.state.menu) ? "show" : "" ;
     }
     
     toggleMenu(){
         console.log(!this.state.menu);
-        this.setState({ menu: !this.state.menu, responsive: !this.state.responsive, isActive: true})
+        console.log(window.innerWidth);
+        if(window.innerWidth >= 770){
+            this.setState({ menu: true, responsive: false})
+        }else{
+            this.setState({ menu: !this.state.menu, responsive: !this.state.responsive})
+        }
     }
 
     render(){
 
         const show = (this.state.menu) ? "show" : "" ;
-        const collap = (!this.state.isActive) ? " hide" : "" ;
+        // const collap = (!this.state.isActive) ? " hide" : "" ;
         // const col_side = (this.state.menu) ? "col-md-2" : "" ;
-
+        console.log("PP ",window.innerWidth);
+        if(window.innerWidth >= 770){
+        }
+        const win = (window.innerWidth >= 770) ? " show2" : "";
 // {"nav-item"+ col_side}
         return(
-            <nav className="navbar navbar-dark mb-5 navbar-right">
+            <nav className="navbar navbar-dark mb-5 navbar-right container">
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="sideBar"
                     aria-controls="sideBar" id="sideBar-toggler" aria-expanded="false" aria-label="Toggle navigation" onClick={ this.toggleMenu }>
@@ -47,8 +59,14 @@ class sideBar extends Component {
                     {/* → */}
                 </button>
 
-                <div className={"sideBar table-wrapper mt-2 mt-lg-0 text-center collapse navbar-collapse " + show+collap}  id="sideBar">
-                    <li className="nav-item">
+            {
+                    console.log("ijnk",window.innerWidth)
+
+            }
+                {
+                    window.innerWidth >= 770 ? 
+                        <div className={"sideBar table-wrapper mt-2 mt-lg-0 text-center collapse navbar-collapse show "}  id="sideBar">
+                            <li className="nav-item">
                     <Link className="nav-link itemSidebar" to="prestamos"><FontAwesomeIcon icon="book"/>Préstamos</Link>
                         <hr/>
                     </li>
@@ -91,6 +109,54 @@ class sideBar extends Component {
                         <hr/>
                     </li>
                 </div>
+                    :
+                        <div className={"sideBar table-wrapper mt-2 mt-lg-0 text-center collapse navbar-collapse  " + show + win}  id="sideBar">
+                            <li className="nav-item">
+                    <Link className="nav-link itemSidebar" to="prestamos"><FontAwesomeIcon icon="book"/>Préstamos</Link>
+                        <hr/>
+                    </li>
+                    {/* <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="historial"><FontAwesomeIcon icon="history"/>Historiales</Link>
+                        <hr/>
+                    </li> */}
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="categoria"><FontAwesomeIcon icon="layer-group"/>Categorías</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="mascarilla"><FontAwesomeIcon icon="head-side-mask"/>Mascarillas</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="mascarillasEntregadas"><FontAwesomeIcon icon="head-side-mask"/>Mascarillas Entregadas</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="estadistica"><FontAwesomeIcon icon="chart-bar"/>Estadísticas</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="inventarioObjetos">
+                            {/* <FontAwesomeIcon icon=faArchive}/> */}
+                            <FontAwesomeIcon icon="folder-open"/>Objetos</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="recordatorio"><FontAwesomeIcon icon="clipboard-list"/>Recordatorios</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="defectuosos"><FontAwesomeIcon icon="exclamation-triangle"/>Defectuosos</Link>
+                        <hr/>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link itemSidebar" to="usuario"><FontAwesomeIcon icon="address-book"/>Usuarios</Link>
+                        <hr/>
+                    </li>
+                </div>
+                }
+
+                    
             </nav>
         )
     }
